63198885d7c90bed4fbade10d8c635319f0a87ca
liy49 <895825110@qq.com>
2023-08-18 11:47:37 +0800

    组件初始化

---
+++ Example/Pods/Target Support Files/Pods-ZQMine_Example/Pods-ZQMine_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZQMine_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZQMine_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-ZQMine_Tests/Pods-ZQMine_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZQMine_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZQMine_Tests
+@end
